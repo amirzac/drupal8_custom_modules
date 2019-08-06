7623f1ad307bd845343064e5123b6eda1c9743b3
Anton Mirzac <anton@netkata.com>
2019-08-06 13:59:44 +0200

    Test drupal middleware

---
+++ web/modules/custom/middleware_module/src/Controller/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\middleware_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class TestController extends ControllerBase {
+
+  public function test() {
+
+  }
+}
